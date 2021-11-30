1d1a6e68519bb579a35ec2e3cae89e8516a7aed5
Carljagerhill <Carl__96@hotmail.com>
2021-11-30 23:31:03 +0100

    fixedAudioAndPlayback

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -1,5 +1,11 @@
 import {atom} from 'recoil';
 
+export const playListState= atom({
+    key: 'playListState',
+    default: null,
+});
+
+
 export const playListIdState = atom({
     key: "playListIdState",
     default: '37i9dQZF1DX6J5NfMJS675'
